e46c49d52a66336089c343e5197ca0bfa445fa73
saloi sbai <saloi.benoum@gmail.com>
2022-11-21 22:05:47 +0100

    adduser ok

--- controllers/user/addUser.php
+++ controllers/user/addUser.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // entetes requises
 // qui peut interroger notre API ==> securité
 header("access-Control-Allow-Origin: *");
@@ -13,20 +13,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $pdo = new Database();
     $db = $pdo->getConnexion();
     $user = new User($db);
+    $salt = "monsalt2022";
 
     // on récupère les informations envoyées
     $donnees = json_decode(file_get_contents("php://input"));
 
-    $user->username = $donnees->username;
-    $user->password = $donnees->password;
-    
+    $password = hash('md5', $salt . htmlentities($donnees->password));
+    $username = $donnees->username;
+
+
+    $user->username = $username;
+    $user->password = $password;
+
     //salé le password
 
 
     if ($user->addUser()) {
         http_response_code(201);
-        echo json_encode(["message" =>"L'ajout a été effectué"]);
-    }else{
+        echo json_encode(["message" => "L'ajout a été effectué"]);
+    } else {
         // ici la création n'a pas fonctionné
         // on envoie un code 503
         http_response_code(503);
@@ -35,14 +40,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 } else {
     http_response_code(405); // TODO: revoir les codes erreurs
     echo json_encode(["message" => "methode not Allowed"]);
-
 }
-    
-
-
-
-
-
-
-
-?>
